84f567456549bee742cd687368b45bfd74edf272
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-03-25 17:39:25 -0300

    Classe da raiz quadrada foi criada

---
+++ raiz_quadrada.py
@@ -0,0 +1,8 @@
+import math
+
+class Raiz_Quadrada:
+    def __init__ (self, radicando, indice):
+        self.radicando = radicando
+
+    def calculo_raiz (self):
+        math.sqrt(self.radicando)
