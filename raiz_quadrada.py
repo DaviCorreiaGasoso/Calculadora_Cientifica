dd6f51f93ca11fd3bcfe9a349beabffbeee21b09
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-03-26 15:26:20 -0300

    Classe raízes foi criada

--- raiz_quadrada.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class Raiz_Quadrada:
-    def __init__ (self, radicando, indice):
-        self.radicando = radicando
-
-    def calculo_raiz (self):
-        math.sqrt(self.radicando)
