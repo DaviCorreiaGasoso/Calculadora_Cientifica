0baee9d81987dc0eaf3125fa344c4716c6437e53
tarques <tarcilatorresmt@gmail.com>
2024-03-26 17:45:32 -0300

    Principal foi criada

---
+++ principal.py
@@ -0,0 +1,32 @@
+import pi
+import operações
+import cosseno
+import ceno
+import raizes
+import Tangente
+    
+print("Bem vindo à calculadora cientifica!")
+print("1. Raiz quadrada")
+print("2. Pi")
+print("3. Seno")
+print("4. Cosseno")
+print("5. Tangente")
+print("6. Operações")
+escolha = input("Escolha alguma operação (1/2/3/4/5/6): ")
+num1 = float (input ("Escolha o numero1: "))
+num2 = float (input ("Escolha o numero2: "))
+
+if escolha == '1':
+    print ("resultado: ", modulo_math.raizes (num1 , num2 ))
+elif escolha == '2': 
+    print ("resultado: ", modulo_math.pi (num1 , num2))
+elif escolha == '3':
+    print ("resultado: ", modulo_math.ceno (num1 , num2))
+elif escolha == '4': 
+    print ("resultado: ", modulo_math.cosseno (num1 , num2))
+elif escolha == '5':
+    print ("resultado: ", modulo_math.tangente (num1 , num2))
+elif escolha == '6':
+    print ("resultado: ", modulo_math.operacoes (num1 , num2))            
+else: 
+    print ("Opção Invalida!")
